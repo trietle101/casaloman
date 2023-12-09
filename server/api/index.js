3ae8d7e88aaf77599a2ee8d90376acbd2dee203e
“Triet <trietle101@gmail.com>
2023-12-09 13:21:13 +0700

    fix vercel

--- server/api/index.js
+++ server/api/index.js
@@ -5,10 +5,10 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const cors = require("cors");
 
-var indexRouter = require("./routes/index");
-var categoriesRouter = require("./routes/categories");
-var productsRouter = require("./routes/products");
-var usersRouter = require("./routes/users");
+var indexRouter = require("../routes/index");
+var categoriesRouter = require("../routes/categories");
+var productsRouter = require("../routes/products");
+var usersRouter = require("../routes/users");
 
 var app = express();
 
