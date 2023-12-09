43f286bd6e5b92b51f70eb487d61a67237db207e
“Triet <trietle101@gmail.com>
2023-12-09 11:53:49 +0700

    update db url

--- server/app.js
+++ server/app.js
@@ -15,10 +15,13 @@ var app = express();
 //connect mongodb
 var mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost:27017/casaloman", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect(
+    "mongodb+srv://trietle101:trietle101@cluster0.gpqr65b.mongodb.net/casaloman?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
   .then(() => console.log(">>>>>>>>>> DB Connected!!!!!!"))
   .catch((err) => console.log(">>>>>>>>> DB Error: ", err));
 
